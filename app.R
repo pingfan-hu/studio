bb20c8d540ce5003abd86e9912e1ecf501fcc927
Pingfan Hu <pingfan0727@gmail.com>
2025-12-28 15:16:50 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,2 +1,14 @@
+# Force installation from online-deployment branch
+# Connect Cloud caches packages, so we must force install from GitHub
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+devtools::install_github(
+  "surveydown-dev/sdstudio",
+  ref = "online-deployment",
+  force = TRUE,
+  upgrade = "never"
+)
+
 library(sdstudio)
 sdstudio::launch()
