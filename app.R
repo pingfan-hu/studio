38ea188640fbf9ac48cbee9b496fd9498902b68c
Pingfan Hu <pingfan0727@gmail.com>
2025-12-28 15:18:18 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,8 +1,13 @@
 # Force installation from online-deployment branch
 # Connect Cloud caches packages, so we must force install from GitHub
+
+# Set CRAN mirror for Connect Cloud
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 if (!requireNamespace("devtools", quietly = TRUE)) {
   install.packages("devtools")
 }
+
 devtools::install_github(
   "surveydown-dev/sdstudio",
   ref = "online-deployment",
